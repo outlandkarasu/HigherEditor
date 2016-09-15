46ee7cbb4896706cb87969d538e071c8c5e9a612
akira_yamaguchi@xenoma.com <akira_yamaguchi@xenoma.com>
2016-09-16 00:49:31 +0900

    issue #7 クリックした座標に点を追加できるようにした。

---
+++ Assets/scripts/EditorController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// エディターシーンのコントローラー
+/// </summary>
+public class EditorController : MonoBehaviour {
+
+    /// <summary>
+    /// 点オブジェクトのひな型
+    /// </summary>
+    public GameObject PointPrefab;
+
+    /// <summary>
+    /// 生成される全オブジェクトの親
+    /// </summary>
+    public Transform Objects;
+
+    // 各レイヤー番号
+    private int xyzLayer_;
+    private int xywLayer_;
+    private int wyzLayer_;
+
+    /// <summary>
+    /// オブジェクト生成時の処理
+    /// </summary>
+    void Awake()
+    {
+        xyzLayer_ = LayerMask.NameToLayer("SpaceXYZ");
+        xywLayer_ = LayerMask.NameToLayer("SpaceXYW");
+        wyzLayer_ = LayerMask.NameToLayer("SpaceWYZ");
+    }
+
+    /// <summary>
+    /// クリック時の処理
+    /// </summary>
+    /// <param name="worldPosition">クリックされたワールド座標</param>
+    /// <param name="layer">クリックされたレイヤー</param>
+    public void OnClick(Vector3 worldPosition, int layer)
+    {
+        // 新しい点の生成
+        GameObject newPoint = Instantiate<GameObject>(PointPrefab);
+        newPoint.transform.parent = Objects;
+        
+        // 4次元位置を設定
+        Vector4 pos = GetPosition4D(worldPosition, layer);
+        newPoint.transform.position = pos;
+        newPoint.GetComponent<PointController>().W = pos.w;
+    }
+
+    // 3次元座標とレイヤーから4次元座標を取得する。
+    private Vector4 GetPosition4D(Vector3 pos, int layer)
+    {
+        if (layer == xyzLayer_)
+        {
+            return new Vector4(pos.x, pos.y, pos.z, 0.0f);
+        }
+        else if (layer == xywLayer_)
+        {
+            return new Vector4(pos.x, pos.y, 0.0f, pos.z);
+        }
+        else
+        {
+            return new Vector4(0.0f, pos.y, pos.z, pos.x);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/SubCameraPanelController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+/// <summary>
+/// カメラ全面パネルコントローラー
+/// </summary>
+public class SubCameraPanelController : MonoBehaviour, IPointerClickHandler
+{
+    // クリック箇所のカメラからの距離
+    private const float CLICK_DISTANCE = 1.0f;
+
+    private Camera camera_;
+    private EditorController editorController_;
+
+    /// <summary>
+    /// オブジェクト生成時の処理
+    /// </summary>
+    void Awake()
+    {
+        camera_ = GetComponentInParent<Camera>();
+        editorController_ = GetComponentInParent<EditorController>();
+    }
+
+    /// <summary>
+    /// クリック時の処理
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Vector3 pos = new Vector3(eventData.position.x, eventData.position.y, 0.0f);
+        Ray ray = camera_.ScreenPointToRay(pos);
+        editorController_.OnClick(ray.GetPoint(CLICK_DISTANCE), gameObject.layer);
+    }
+}
